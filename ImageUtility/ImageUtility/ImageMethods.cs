a81895e2ef056bc524905dc305e565ceff4ab7c3
RomainAI <47427890+RomainAI@users.noreply.github.com>
2019-05-27 13:37:54 +0100

    Add files via upload

---
+++ ImageUtility/ImageUtility/ImageMethods.cs
@@ -0,0 +1,87 @@
+﻿using AForge;
+using AForge.Imaging.Filters;
+using System;
+using System.Drawing;
+
+namespace ImageUtility
+{
+    public class ImageMethods
+    {
+
+        private static Bitmap applyFilterMedian(Bitmap img)
+        {
+            Median filter = new Median();
+            return filter.Apply(img);
+        }
+
+        private static Bitmap applyFilterAdaptiveSmoothing(Bitmap img)
+        {
+            AdaptiveSmoothing filter = new AdaptiveSmoothing();
+            return filter.Apply(img);
+        }
+
+        private static Bitmap applyFilterGrayscale(System.Drawing.Bitmap img)
+        {
+            Grayscale filter = new Grayscale(0.2125, 0.7154, 0.0721);
+            return filter.Apply(img);
+        }
+
+        private static Bitmap applyFilterHslLinear(Bitmap img)
+        {
+            HSLLinear filter = new HSLLinear();
+            filter.InLuminance = new Range(0, 0.85f);
+            filter.OutSaturation = new Range(0.25f, 1);
+            return filter.Apply(img);
+        }
+
+        private static Bitmap applyFilterBradleyLocalThresholding(Bitmap image)
+        {
+            BradleyLocalThresholding filter = new BradleyLocalThresholding();
+            return filter.Apply(image);
+        }
+
+        /// <summary>
+        /// Apply some filters to an image to increase OCR extraction result 
+        /// </summary>
+        /// <param name="applyHslLinear">1=yes, 0=no</param>
+        /// <param name="applyGrayScale">1=yes, 0=no</param>
+        /// <param name="applyMedian">1=yes, 0=no</param>
+        /// <param name="applyAdaptiveSmoothing">1=yes, 0=no</param>
+        /// <param name="imageSourceFile">Image source file path</param>
+        /// <param name="imageResultFile">Image result file path</param>
+        /// <returns>OK or execption message</returns>
+        public static string applyFilterToImage(string applyHslLinear, string applyGrayScale, string applyMedian, string applyAdaptiveSmoothing, string imageSourceFile, string imageResultFile)
+        {
+            string returnResult = "OK";
+
+            try
+            {
+                System.Drawing.Bitmap img = new System.Drawing.Bitmap(imageSourceFile);
+
+                if (applyHslLinear == "1")
+                    img = applyFilterHslLinear(img);
+
+                if (applyGrayScale == "1")
+                    img = applyFilterGrayscale(img);
+
+                if (applyMedian=="1")
+                    img = applyFilterMedian(img);
+
+                if (applyAdaptiveSmoothing == "1")
+                    img = applyFilterAdaptiveSmoothing(img);
+
+                img.Save(imageResultFile);
+            }
+            catch (Exception ex)
+            {
+                returnResult = ex.Message;
+            }
+
+            return returnResult;
+            
+        }
+
+    }
+
+    
+}
---
+++ ImageUtility/ImageUtility/PdfMethods.cs
@@ -0,0 +1,47 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.IO;
+
+namespace ImageUtility
+{
+    public class PdfMethods
+    {
+
+        /// <summary>
+        /// Convert a image file to a PDF file
+        /// </summary>
+        /// <param name="imageFilePath">Image file path</param>
+        /// <param name="pdfFilePath">PDF file path</param>
+        public static string convertImageToPDF(string imageFilePath, string pdfFilePath)
+        {
+            string returnResult = "OK";
+
+            try
+            {
+                iTextSharp.text.Image image = iTextSharp.text.Image.GetInstance(imageFilePath);
+                using (FileStream fs = new FileStream(pdfFilePath, FileMode.Create, FileAccess.Write, FileShare.None))
+                {
+                    using (Document doc = new Document(image))
+                    {
+                        using (PdfWriter writer = PdfWriter.GetInstance(doc, fs))
+                        {
+                            doc.Open();
+                            image.SetAbsolutePosition(0, 0);
+                            writer.DirectContent.AddImage(image);
+                            doc.Close();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                returnResult = ex.Message;
+            }
+
+            return returnResult;
+            
+        }
+
+    }
+}
